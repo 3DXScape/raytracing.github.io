74c21f7ddbabe1632827021651ab168199175bd8
3DXScape <steve@opensiteplan.org>
2022-10-16 10:44:55 -0700

    with triangle

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -54,9 +54,11 @@ hittable_list random_scene() {
     hittable_list world;
 
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    auto triangle_material = make_shared<lambertian>(color(1.0, 0.0, 0.0));
+    world.add(make_shared<triangle>(point3(0, 0.5, 0), point3(0.4, 0.3, 0.4), point3(0.1, 0.40, 0.6), triangle_material));
 
-    for (int a = -11; a < 11; a++) {
+    for (int a = -11; false && a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
             point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
@@ -85,13 +87,13 @@ hittable_list random_scene() {
     }
 
     auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    //world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
     auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+    //world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
     auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+    //world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
     return world;
 }
@@ -102,7 +104,7 @@ int main(int argc, char* argv[]) {
     // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 2400;
+    const int image_width = 600;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 10;
     const int max_depth = 50;
